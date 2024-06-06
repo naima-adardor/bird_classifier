dbed01816783c106205cfe48fd703c8f5ebfbcb5
Adardor Naima <naima.adardor@edu.uiz.ac.ma>
2024-06-06 18:00:33 +0000

    adds

--- bird.py
+++ bird.py
@@ -162,50 +162,50 @@ if audio_file is not None:
     label_info = class_mapping_data[class_mapping_data['encoded_label'] == prediction[0]].iloc[0]
     st.write(f"The bird is: {label_info['scientific_name']}")
 
-# # Fetch Wikipedia information and the first image
-#     wiki_title, wiki_info = fetch_wikipedia_info(label_info['scientific_name'])
-#     st.write(f"**{wiki_title}**")
-#     st.write(f"{wiki_info}")
+# Fetch Wikipedia information and the first image
+    wiki_title, wiki_info = fetch_wikipedia_info(label_info['scientific_name'])
+    st.write(f"**{wiki_title}**")
+    st.write(f"{wiki_info}")
 
-#     # Create a DataFrame for plotting
-#     tmp = class_mapping_data[class_mapping_data['encoded_label'] == prediction[0]].copy()
+    # Create a DataFrame for plotting
+    tmp = class_mapping_data[class_mapping_data['encoded_label'] == prediction[0]].copy()
     
-#     # Create a scatter mapbox plot
-#     fig = px.scatter_mapbox(
-#         tmp,
-#         lat="latitude",
-#         lon="longitude",
-#         color="primary_label",
-#         zoom=10,
-#         title='Bird Recordings Location'
-#     )
+    # Create a scatter mapbox plot
+    fig = px.scatter_mapbox(
+        tmp,
+        lat="latitude",
+        lon="longitude",
+        color="primary_label",
+        zoom=10,
+        title='Bird Recordings Location'
+    )
 
-#     # Update the layout of the plot to use the "open-street-map" style for the map background
-#     fig.update_layout(mapbox_style="open-street-map")
+    # Update the layout of the plot to use the "open-street-map" style for the map background
+    fig.update_layout(mapbox_style="open-street-map")
 
-#     # Update the layout of the plot to set the margin around the map
-#     fig.update_layout(margin={"r":0,"t":30,"l":0,"b":0})
+    # Update the layout of the plot to set the margin around the map
+    fig.update_layout(margin={"r":0,"t":30,"l":0,"b":0})
 
-#     # Display the scatter mapbox plot
-#     st.plotly_chart(fig, use_container_width=True)
-# # Apply CSS to modify button appearance
-# button_style = """
-#     <style>
-#         div.stButton > button {
-#             background-color: #e59A62;
-#             color: black;
-#             height: 50px;
-#             width: 200px;
-#             font-size: 16px;
-#             border-radius: 10px;
-#             border: none;
-#             cursor: pointer;
-#             item-align: center;
-#         }
-#         div.stButton > button:hover {
-#             background-color: #A75211;
-#             color: white;
-#         }
-#     </style>
-# """
-# st.markdown(button_style, unsafe_allow_html=True)
+    # Display the scatter mapbox plot
+    st.plotly_chart(fig, use_container_width=True)
+# Apply CSS to modify button appearance
+button_style = """
+    <style>
+        div.stButton > button {
+            background-color: #e59A62;
+            color: black;
+            height: 50px;
+            width: 200px;
+            font-size: 16px;
+            border-radius: 10px;
+            border: none;
+            cursor: pointer;
+            item-align: center;
+        }
+        div.stButton > button:hover {
+            background-color: #A75211;
+            color: white;
+        }
+    </style>
+"""
+st.markdown(button_style, unsafe_allow_html=True)
