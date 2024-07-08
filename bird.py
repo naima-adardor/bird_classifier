dbd385ebd0db6b257f1d7aa321d2dd94da586157
Adardor Naima <naima.adardor@edu.uiz.ac.ma>
2024-07-08 12:30:45 +0100

    add nootebook

--- bird.py
+++ bird.py
@@ -9,28 +9,25 @@ import plotly.express as px
 import wikipedia
 from io import BytesIO
 import soundfile as sf
+import matplotlib.pyplot as plt
+import librosa.display
 
 # Function to fetch Wikipedia information
 @st.cache_data
 def fetch_wikipedia_info(bird_name):
     try:
         page = wikipedia.page(bird_name)
-        # Get the first section or big title
-        first_section = next(iter(page.sections), None)
-        if first_section:
-            title = first_section.title
-            text = first_section.text
-            return title, text
-        else:
-            return bird_name, page.summary  # Fallback to summary if no section is found
+        title = page.title
+        summary = page.summary
+        images = page.images[:1]  # Get the first image only
+        return title, summary, images
     except wikipedia.exceptions.PageError:
-        return bird_name, "No information available on Wikipedia."
+        return bird_name, "No information available on Wikipedia.", []
 
 # Function to normalize and denoise audio
+@st.cache_data
 def normalize_and_denoise(audio, sample_rate):
-    # Normalize audio data
     audio = audio / np.max(np.abs(audio))
-    # Apply Spectral Subtraction Filter
     filtered_audio = nr.reduce_noise(y=audio, sr=sample_rate)
     return filtered_audio
 
@@ -41,6 +38,9 @@ def extract_features(file_path):
     mfccs = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
     flattened_features = np.mean(mfccs.T, axis=0)
     return flattened_features, audio, sample_rate
+
+# Function to plot spectrogram
+
 
 # Set page configuration
 st.set_page_config(
@@ -93,56 +93,64 @@ audio_file = st.file_uploader("Upload an audio file (ogg, mp3, wav)", type=["ogg
 
 # Check if audio file is uploaded
 if audio_file is not None:
-    # Extract features from the uploaded audio file
     features, audio, sample_rate = extract_features(audio_file)
     features = np.array(features).reshape(1, -1)
-    
-    # Normalize and denoise the audio
     filtered_audio = normalize_and_denoise(audio, sample_rate)
     
-    # Play the original and filtered audio files
+    # Display metadata
+    st.write(f"**Audio Metadata:**")
+    st.write(f"- Duration: {librosa.get_duration(y=audio, sr=sample_rate):.2f} seconds")
+    st.write(f"- Sample Rate: {sample_rate} Hz")
+
+    # Play original and filtered audio
     st.text("Playing original uploaded audio...")
     st.audio(audio_file)
-
-    # Convert filtered audio to WAV format for playback
     audio_buffer = BytesIO()
     sf.write(audio_buffer, filtered_audio, sample_rate, format='wav')
     audio_buffer.seek(0)
     st.text("Playing filtered audio...")
     st.audio(audio_buffer)
 
-    # Load the trained model and class mapping data
+
+    # Load model and predict
     model = joblib.load('audio_classifier_model.joblib')
     class_mapping_data = pd.read_csv('unique_merged_data.csv')  # Adjust the path as necessary
-
-    # Predict the bird species
     prediction = model.predict(features)
     label_info = class_mapping_data[class_mapping_data['encoded_label'] == prediction[0]].iloc[0]
-
-    # Display the predicted bird species
     st.write(f"The predicted bird species is: {label_info['scientific_name']}")
 
-    # Fetch Wikipedia information for the predicted bird species
-    wiki_title, wiki_info = fetch_wikipedia_info(label_info['scientific_name'])
-    st.subheader(f"Wikipedia Information: {wiki_title}")
-    st.write(wiki_info)
+    # Fetch Wikipedia information
+    # Fetch Wikipedia information
+    wiki_title, wiki_info, wiki_images = fetch_wikipedia_info(label_info['scientific_name'])
+    
+    # Create HTML content for displaying text and images side by side
+    images_html = ""
+    for img_url in wiki_images:
+        images_html += f'<img src="{img_url}" style="width: 200px; margin-left: 10px; margin-top:60px;"/>'
+    
+    st.markdown(f"""
+        <div style="display: flex; align-items: flex-start;">
+            <div style="flex: 1;">
+                <h2>Wikipedia Information: {wiki_title}</h2>
+                <p>{wiki_info}</p>
+            </div>
+            <div style="flex: 0 0 auto;">
+                {images_html}
+            </div>
+        </div>
+    """, unsafe_allow_html=True)
 
-    # Filter the data to include only the recordings of the predicted bird species
+
+    # Filter data for map
     filtered_data = class_mapping_data[class_mapping_data['encoded_label'] == prediction[0]]
-
-    # Define the boundaries for the Western Ghats region
     lower_latitude = 8
     upper_latitude = 22
     lower_longitude = 73
     upper_longitude = 79
-
-    # Further filter the data to include only recordings within the Western Ghats region
     filtered_data = filtered_data[(filtered_data['latitude'] >= lower_latitude) & 
                                   (filtered_data['latitude'] <= upper_latitude) &
                                   (filtered_data['longitude'] >= lower_longitude) &
                                   (filtered_data['longitude'] <= upper_longitude)]
-
-    # Create a scatter mapbox plot for the filtered data
     fig = px.scatter_mapbox(
         filtered_data,
         lat="latitude",
@@ -154,3 +162,4 @@ if audio_file is not None:
     )
     fig.update_layout(mapbox_style="open-street-map", margin={"r":0, "t":30, "l":0, "b":0})
     st.plotly_chart(fig, use_container_width=True)
+
